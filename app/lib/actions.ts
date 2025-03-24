fc5fee9cf2982e7b4cad9028bff4ed12f9e1e702
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-03-24 23:48:46 +0100

    feat: Mutating Data - form Actions

---
+++ app/lib/actions.ts
@@ -0,0 +1,42 @@
+"use server";
+import { z } from "zod";
+
+import postgres from "postgres";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+if (!process.env.POSTGRES_URL) {
+  throw new Error("POSTGRES_URL environment variable is not defined");
+}
+
+const sql = postgres(process.env.POSTGRES_URL, { ssl: { rejectUnauthorized: false } });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.string(),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  // console.log(rawFormData)
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+
+INSERT INTO invoices (customer_id, amount, status, date)
+VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+
+`;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
